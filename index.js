4ed5d6a3a43a191c1bf8d557dbbb22f766225834
wakio-ngari <ngarijoyce017@gmail.com>
2024-11-27 18:16:46 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
-// Write your code here
+let num1 =31;
+let num2=2;
+  let multiply =num1*num2 ;
+console.log(multiply);
+
+const random =Math.floor(Math.random() * 100) + 1;
+console.log (random);
+
+
+let num3 =14;
+let num4 =10;
+
+let mod = num3 % num4;
+console.log(mod);
+
+
+
+const numbers=[2,5,20,10,5];
+ const max=Math.max(...numbers);
+ console.log(max);
+
